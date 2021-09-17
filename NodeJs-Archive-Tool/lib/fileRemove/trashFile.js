eb2521ff87b3ddeae6321a32e7a5fab2b72b6180
Pulipuli Chen <pulipuli.chen@gmail.com>
2021-09-17 16:14:09 +0800

    0917 Linux

--- NodeJs-Archive-Tool/lib/fileRemove/trashFile.js
+++ NodeJs-Archive-Tool/lib/fileRemove/trashFile.js
@@ -11,11 +11,20 @@ module.exports = async function (file) {
   let i = 0
   while (i < 3) {
     try {
-      await trash(file, {
-        glob: false
-      })
+      let platform = await os.platform()
+      //console.log()
+      
+      try {
+        await trash(file, {
+          glob: false
+        })
+      }
+      catch (e) {
+        await execShellCommand('trash ' + file)
+      }
       break
     } catch (e) {
+      console.error(e, file)
       await sleep(3000)
       i++
     }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NodeJs-Archive-Tool
 Cross-platform tools to archive files and build file lists.
+
+# Linux
+
+需要先確認7z有安裝
