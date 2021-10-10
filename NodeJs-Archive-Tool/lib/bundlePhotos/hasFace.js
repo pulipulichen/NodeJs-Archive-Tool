7f642249f986c4c00f232f045bbf056de8b3f315
Pulipuli Chen <blog@pulipuli.info>
2021-10-10 20:14:01 +0800

    1010 要修復在Docker上使用TF的問題

--- NodeJs-Archive-Tool/lib/bundlePhotos/hasFace.js
+++ NodeJs-Archive-Tool/lib/bundlePhotos/hasFace.js
@@ -43,14 +43,19 @@ const initFaceapi = async function () {
     return true
   }
   
+  console.log('initFaceapi 1')
+  
   tf = require('@tensorflow/tfjs-node')
   faceapi = require('@vladmandic/face-api');
+  
+  console.log('initFaceapi 2')
   
     await faceapi.tf.setBackend("tensorflow");
     await faceapi.tf.enableProdMode();
     await faceapi.tf.ENV.set("DEBUG", false);
     await faceapi.tf.ready();
-
+    
+console.log('initFaceapi 3')
   //  console.log(
   //    `Version: TensorFlow/JS ${faceapi.tf?.version_core} FaceAPI ${
   //      faceapi.version.faceapi
@@ -65,31 +70,43 @@ const initFaceapi = async function () {
     tinyFaceDetector = new faceapi.TinyFaceDetectorOptions({
       minConfidence: 0.5,
     })
-    
+  
+  console.log('initFaceapi 4')
     faceAPIisReady = true
 }
 
 const hasFace = async function (input) {
+  console.log('hasFace 1')
   await initFaceapi()
+  
+  console.log('hasFace 2')
   try {
     //let input = '/home/pudding/Documents/NetBeansProjects/[nodejs]/NodeJs-Archive-Tool/NodeJs-Archive-Tool/[test/20211009 photo/20211009 a/LINE_1578402778237.jpg'
     //console.log('[gogogo]', input)
     input = fs.readFileSync(input)
+    
+    console.log('hasFace 3')
     //const detection = await faceapi.detectSingleFace(input)
     //console.log(detection)
 
+    console.log('hasFace 4')
 
     const tensor = await image(input);
     const result = await detect(tensor);
     //console.log("Detected faces:", result.length);
 
+    console.log('hasFace 5')
+
     tensor.dispose();
+
+    console.log('hasFace 6')
 
     return (result.length > 0)
     //const { Canvas, Image, ImageData } = canvas
     //faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
   }
   catch (e) {
+    console.log('hasFace 7')
     return false
   }
 }
